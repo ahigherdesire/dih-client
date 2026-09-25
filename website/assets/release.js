5823e1028f8929a7f98232d7a836793302a17e88
ahigherdesire <h2510079@nushigh.edu.sg>
2026-09-25 12:23:25 +0800

    Site: SHA-256 for the jar with the update tracker

--- website/assets/release.js
+++ website/assets/release.js
@@ -4,7 +4,7 @@ window.DIH_RELEASE = {
   mc: "26.2",
   file: "DIH-Client-5.0-26.2.jar",
   size: "20.8 MB",
-  sha256: "a92e39eb2f5067a72038e30ab0311261cc93c94c94092d309d2d0e7ec6b3a975",
+  sha256: "232f2bb2f0dd48a4ba9b5a33c0214649877d9d97b9066479312b7d39d890f4fd",
   repo: "https://github.com/ahigherdesire/dih-client",
   download: "https://github.com/ahigherdesire/dih-client/releases/download/v5.0/DIH-Client-5.0-26.2.jar",
   /* JourneyMap can't be bundled (All Rights Reserved), so link Modrinth's own file. */
